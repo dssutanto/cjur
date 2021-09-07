26f58bcf7e74f7e93fda92c7ba9cf9b82993c275
dssutanto <60329709+dssutanto@users.noreply.github.com>
2021-09-07 00:45:31 -0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://canadian-journal-of-undergraduate-research.local/graphql`,
+          `https://cjur.ca/graphql`,
         auth: {
           htaccess: {
             username: `cjuresearchadmin`,
